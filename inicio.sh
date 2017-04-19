6d562b70ccad9bb330bbfc5a4207a8352ace89ab
Javier Santa Cruz <correo@javistacruz.com>
2017-04-19 20:14:03 +0200

    sangrado correcto

--- inicio.sh
+++ inicio.sh
@@ -22,93 +22,93 @@ read op
 case $op in 
 
     1)
-function mostrar (){
-printf "%0"$ceros"d \t \n" $sumando
-}
-function solucion (){
+	function mostrar (){
+	    printf "%0"$ceros"d \t \n" $sumando
+	}
+	function solucion (){
 
-echo "---------"
-}
+	    echo "---------"
+	}
 
-;;
+	;;
     2)
-function mostrar (){
-clear
-sleep 0.1
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo "                                $sumando"
+	function mostrar (){
+	    clear
+	    sleep 0.1
+	    echo ""
+	    echo ""
+	    echo ""
+	    echo ""
+	    echo ""
+	    echo ""
+	    echo ""
+	    echo "                                $sumando"
 
-sleep $temp
-}
+	    sleep $temp
+	}
 
-function solucion (){   
-clear
-echo "Respuesta:"
-}
-    ;;
+	function solucion (){   
+	    clear
+	    echo "Respuesta:"
+	}
+	;;
 
     3)
-echo "¿Cuántos sumandos quiere que muestre anzan?"
-read limite
-echo "¿Cuántas cifras quiere que tenga cada sumando?"
-read ceros
-case $ceros in
-1)
-cifras=9
-;;
+	echo "¿Cuántos sumandos quiere que muestre anzan?"
+	read limite
+	echo "¿Cuántas cifras quiere que tenga cada sumando?"
+	read ceros
+	case $ceros in
+	    1)
+		cifras=9
+		;;
 
-2)
-cifras=99
-;;
+	    2)
+		cifras=99
+		;;
 
-3)
-cifras=999
-;;
+	    3)
+		cifras=999
+		;;
 
-4)
-cifras=9999
-;;
+	    4)
+		cifras=9999
+		;;
 
-5)
-cifras=99999
-;;
+	    5)
+		cifras=99999
+		;;
 
-6)
-temp=999999
-;;
+	    6)
+		temp=999999
+		;;
 
-*)
-printf "No vas a poder con eso, te pongo algo más asequible..."
-cifras=999999
-;;
-esac
-echo "¿Cuánto tiempo quiere que le muestre cada sumando? (este parámetro sólo es válido en flash anzan)" 
-echo "*0.5 es medio segundo. Recuerde usar el punto y no la coma*"
-read temp
-echo "¿Cuántos segundos necesita para calcular el total?"
-echo "*0.5 es medio segundo. Recuerde usar el punto y no la coma*"
-read solutemp
-#y ahora lo mandamos todo al archivo de configuración.
-echo "#!/bin/bash">confi
-echo "limite=$limite">>confi
-echo "ceros=$ceros">>confi
-echo "cifras=$cifras">>confi
-echo "temp=$temp">>confi
-echo "solutemp=$solutemp">>confi
-bash inicio.sh
-    ;;
+	    *)
+		printf "No vas a poder con eso, te pongo algo más asequible..."
+		cifras=999999
+		;;
+	esac
+	echo "¿Cuánto tiempo quiere que le muestre cada sumando? (este parámetro sólo es válido en flash anzan)" 
+	echo "*0.5 es medio segundo. Recuerde usar el punto y no la coma*"
+	read temp
+	echo "¿Cuántos segundos necesita para calcular el total?"
+	echo "*0.5 es medio segundo. Recuerde usar el punto y no la coma*"
+	read solutemp
+	#y ahora lo mandamos todo al archivo de configuración.
+	echo "#!/bin/bash">confi
+	echo "limite=$limite">>confi
+	echo "ceros=$ceros">>confi
+	echo "cifras=$cifras">>confi
+	echo "temp=$temp">>confi
+	echo "solutemp=$solutemp">>confi
+	bash inicio.sh
+	;;
 
-*)
-echo "usted ha seleccionado una opción que no existe"
-sleep 1
-bash inicio.sh
-;;
+    *)
+	echo "usted ha seleccionado una opción que no existe"
+	sleep 1
+	bash inicio.sh
+	;;
 esac 
 
 #lanzamos la función anzan.
